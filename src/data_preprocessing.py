e13c914ccfa2280562438801bb6456d25ab975c2
varshitha kadiyam <varshithakadiyam05@gmail.com>
2026-03-11 11:57:00 +0530

    intial commit

---
+++ src/data_preprocessing.py
@@ -0,0 +1,15 @@
+#1. load raw data
+#2. identifying X and y(input or output)
+#3. split data into train and test set
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.model_selection import train_test_split
+ 
+def load_and_Split_data():
+    data=pd.read_csv("../data/raw/insurance_data.csv")
+    X=data[['Age','Annual_Income_LPA','Policy_Term_Years','Sum_Assured_Lakhs']]
+    y=data['Annual_Premium_Thousands']
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
+    return X_train,X_test,y_train,y_test
+ 
